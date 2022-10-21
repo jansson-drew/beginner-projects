3d6d60b0387e710165693eb23b8e71da01429e11
jansson-drew <112445328+jansson-drew@users.noreply.github.com>
2022-10-21 15:56:08 -0400

    Add files via upload

---
+++ chartype.py
@@ -0,0 +1,39 @@
+char1 = input("Enter a character: ")
+if(char1.islower() == True):
+    print(char1, "is a lower case letter.")
+elif(char1.isdigit() == True):
+    print(char1, "is a digit.")
+elif(char1.isalnum() == False):
+    print(char1, "is a non-alphanumeric character.")
+elif(char1.isupper() == True):
+    print(char1, "is an upper case letter.")
+
+char2 = input("Enter a character: ")
+if(char2.islower() == True):
+    print(char2, "is a lower case letter.")
+elif(char2.isdigit() == True):
+    print(char2, "is a digit.")
+elif(char2.isalnum() == False):
+    print(char2, "is a non-alphanumeric character.")
+elif(char2.isupper() == True):
+    print(char2, "is an upper case letter.")
+
+char3 = input("Enter a character: ")
+if(char3.islower() == True):
+    print(char3, "is a lower case letter.")
+elif(char3.isdigit() == True):
+    print(char3, "is a digit.")
+elif(char3.isalnum() == False):
+    print(char3, "is a non-alphanumeric character.")
+elif(char3.isupper() == True):
+    print(char3, "is an upper case letter.")
+
+char4 = input("Enter a character: ")
+if(char4.islower() == True):
+    print(char4, "is a lower case letter.")
+elif(char4.isdigit() == True):
+    print(char4, "is a digit.")
+elif(char4.isalnum() == False):
+    print(char4, "is a non-alphanumeric character.")
+elif(char4.isupper() == True):
+    print(char4, "is an upper case letter.")
